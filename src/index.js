0c4c98bdda8e58d57ea6aa9a9c2a595df9532356
Orakpo Smith <smithorakpo@gmail.com>
2022-04-11 19:29:31 +0100

    scaffold react app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+const app = document.getElementById('app')
+const root = createRoot(app)
+
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
